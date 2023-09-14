90145257911d79a4061b0f8ae420562f3e2b90f3
Bartosz <bartosz.slowik@student.pk.edu.pl>
2023-09-14 22:38:39 +0200

    changed format!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() -> eyre::Result<()> {
     // read env variables that were set in build script
     let bios_path = env!("BIOS_PATH");
     let cargo_dir = env!("CARGO_MANIFEST_DIR");
-    let bootsplash_path = format!("{}/src/assets/bootsplash.bmp", cargo_dir);
+    let bootsplash_path = format!("{cargo_dir}/src/assets/bootsplash.bmp");
     std::process::Command::new("qemu-system-x86_64")
         .arg("-drive")
         .arg(format!("format=raw,file={bios_path}"))
