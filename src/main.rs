78e77cbabe7208114bf529be81aef825401fcb8d
Bartosz <bartosz.slowik@student.pk.edu.pl>
2023-09-14 22:51:26 +0200

    made pretteir format

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ fn main() -> eyre::Result<()> {
     // read env variables that were set in build script
     let bios_path = env!("BIOS_PATH");
     let cargo_dir = env!("CARGO_MANIFEST_DIR");
-    let bootsplash_argument =  format!("menu=on,splash={cargo_dir}/src/assets/bootsplash.bmp,splash-time=1000");
+    let bootsplash_path = format!("{cargo_dir}/src/assets/bootsplash.bmp");
+
     std::process::Command::new("qemu-system-x86_64")
         .arg("-drive")
         .arg(format!("format=raw,file={bios_path}"))
         .arg("-boot")
-        .arg(bootsplash_argument)
+        .arg(format!("menu=on,splash={bootsplash_path},splash-time=1000"))
         .spawn()?
         .wait()?;
 
