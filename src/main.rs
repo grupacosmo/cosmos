abfe70684cd66066bcb777899474dfc723ff1e2d
Bartosz <bartosz.slowik@student.pk.edu.pl>
2023-09-14 22:28:03 +0200

    added splashscreen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 fn main() -> eyre::Result<()> {
     // read env variables that were set in build script
     let bios_path = env!("BIOS_PATH");
-
+    let cargo_dir = env!("CARGO_MANIFEST_DIR");
     std::process::Command::new("qemu-system-x86_64")
         .arg("-drive")
         .arg(format!("format=raw,file={bios_path}"))
+        .arg("-boot")
+        .arg( format!("menu=on,splash={}\\src\\assets\\bootsplash.bmp,splash-time=1000", cargo_dir))
         .spawn()?
         .wait()?;
 
