d1b84bb3d3d7fc9c0187baee060acc6d72efbf65
Vixu <lonevixu@gmail.com>
2023-09-16 23:39:18 +0200

    Revert "initialize gdt and idt"
    This reverts commit 353ba83f0dfc8fbc9d10aa09c027a80d81f67f79.

--- kernel/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![no_std]
-#![no_main]
-#![feature(abi_x86_interrupt)]
-
-pub mod interrupt;
-pub mod logger;
-pub mod vga;
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,31 +1,27 @@
 #![no_std]
 #![no_main]
-#![feature(abi_x86_interrupt)]
 
 use bootloader_api::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use kernel::{interrupt, logger, println, vga};
+
+mod logger;
+mod vga;
 
 entry_point!(main);
 
 fn main(boot_info: &'static mut BootInfo) -> ! {
-    interrupt::init();
-
     let framebuffer = boot_info.framebuffer.as_mut().unwrap();
     let vga = vga::Writer::new(framebuffer);
 
     logger::init_global(vga);
 
-    // FIXME: cleanup main once we have a test harness
+    // dupa driven development
+    println!("dupa");
+
     for num in 0..50 {
         println!("line {}", num)
     }
     println!("end");
-
-    fn stack_overflow() {
-        stack_overflow();
-    }
-    stack_overflow();
 
     loop {}
 }
